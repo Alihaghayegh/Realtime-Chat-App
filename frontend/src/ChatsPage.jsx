7e41ace909bb0584fab5121387b3f1dcce195084
alihaghayegh <haghayeghali95@gmail.com>
2023-09-09 17:52:10 +0330

    ChatsPage.jsx and frontend completed

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { MultiChatSocket, MultiChatWindow, useMultiChatLogic } from 'react-chat-engine-advanced';
 
-function ChatsPage() {
+
+function ChatsPage(props) {
+  const chatProps = useMultiChatLogic(`${process.env.API_KEY}`, props.user.username, props.user.secret)
   return (
-    <div>ChatsPage</div>
+    <div style={{height: '100vh'}}>
+      <MultiChatSocket {...chatProps} />
+      <MultiChatWindow {...chatProps} style={{height: '100%'}} />
+    </div>
   )
 }
 
